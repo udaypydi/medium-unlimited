f014ffbc0f8a8f05a45fb46df586cf68e7613335
udaypydi <udaypydi333@gmail.com>
2020-03-08 22:04:10 +0530

    add script to read and update local storage

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.get(['post-article|posts-viewed-count'], function(items) {
-        message('articles', items);
+    chrome.storage.local.get(['post-article|posts-viewed-count'], function(items) {
+        if (items > 3) {
+            chrome.storage.local.set({'post-article|posts-viewed-count': 0}, function() {
+                console.log('Value is set to ' + 0);
+            });
+        }
     });
 });
