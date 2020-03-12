572194625397b699a767693654293d265754a32a
udaypydi <udaypydi333@gmail.com>
2020-03-12 21:22:49 +0530

    Update background.js

--- background.js
+++ background.js
@@ -10,6 +10,9 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
         
         chrome.cookies.getAll({domain: "medium.com"}, function(cookies) {
             for(var i=0; i<cookies.length;i++) {
+                
+                // delete every cookie
+                
                 chrome.cookies.remove({url: "https://medium.com/" + cookies[i].path, name: cookies[i].name});
             }
         });
