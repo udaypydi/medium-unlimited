3cde0423c9ca2bdff4b802ac4bcbf28b965eabd5
Harish Mamidi <harish.mamidi.kumar@gmail.com>
2022-03-14 08:52:08 +0530

    Updated to manifest V3

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    
-    // call the hook once the tab is completely loaded.
-    
-    if (changeInfo.status === 'complete') {
-        chrome.cookies.getAll({domain: "medium.com"}, (cookies) => {
-            for(let i = 0; i < cookies.length; i++) {
-                chrome.cookies.remove({url: "https://medium.com/" + cookies[i].path, name: cookies[i].name});
-            }
+  // call the hook once the tab is completely loaded.
+
+  if (changeInfo.status === "complete") {
+    chrome.cookies.getAll({ domain: "medium.com" }, (cookies) => {
+      for (let i = 0; i < cookies.length; i++) {
+        chrome.cookies.remove({
+          url: "https://medium.com/" + cookies[i].path,
+          name: cookies[i].name,
         });
       }
     });
